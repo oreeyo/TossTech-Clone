21dd41041ee80e0f507c76563680521af566b570
oreeyo00 <oreeyo00@gmail.com>
2024-01-03 18:30:24 +0900

    컨트롤러 추가 및 app.js 수정

--- src/app.js
+++ src/app.js
@@ -1,19 +1,3 @@
-import { html } from './utils'
-import logoImage from '../public/images/logo-image.png'
-import { createRouter } from './router'
+import { initialize } from '../src/controllers/Controller.js'
 
-const app = document.getElementById('root')
-app.innerHTML = html`
-	<section>
-		<header class="main-header">
-			<div class="header-container">
-				<div class="logo-image">
-					<img alt="로고 이미지" src="${logoImage}" />
-				</div>
-			</div>
-		</header>
-	</section>
-	<section>
-		<div class="content" id="content"></div>
-	</section>
-`
+document.addEventListener('DOMContentLoaded', initialize)
