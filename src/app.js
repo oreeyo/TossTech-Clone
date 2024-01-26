869d4e02bfeff329213321d320ebcd0d10a5cfd3
oreeyo00 <oreeyo00@gmail.com>
2024-01-26 16:37:41 +0900

    app.js 에서는 initialize 함수만 담당하도록 역할 분리

--- src/app.js
+++ src/app.js
@@ -1,8 +1,3 @@
 import { initialize } from '../src/controllers/Controller.js'
-import { ContentController } from '../src/controllers/ContentController.js'
 
-document.addEventListener('DOMContentLoaded', () => {
-	initialize()
-	const ContentController_ = new ContentController()
-	ContentController_.init()
-})
+document.addEventListener('DOMContentLoaded', () => initialize())
